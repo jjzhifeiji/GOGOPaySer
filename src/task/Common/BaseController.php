0587fca19b9bebb5d42be323c781c295c481114b
zhifeiji <895807497@qq.com>
2023-06-21 14:54:36 +0800

    invitation

--- src/task/Common/BaseController.php
+++ src/task/Common/BaseController.php
@@ -11,7 +11,7 @@ use PhalApi\Api;
 class BaseController extends Api
 {
 
-    protected function api_success($data)
+    protected function api_success($data = '')
     {
         return $data;
     }
